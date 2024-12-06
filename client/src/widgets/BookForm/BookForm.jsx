bd4af4b3041046db44a07722d91eca5cf3f9d79b
Rasul608 <bakalaevrasul7.ru@xmail.ru>
2024-12-06 16:09:39 +0300

    Book_Form

--- client/src/widgets/BookForm/BookForm.jsx
+++ client/src/widgets/BookForm/BookForm.jsx
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../shared/ui/Button/Button';
+import { message as antMessage } from 'antd';
+import BookApi from '../../entities/book/BookApi';
 
-function BookForm(props) {
-    return (
+export default function BookForm({setBooks, setLoading}) {
+    const [imputs, setImputs] = useState({author: '', title: '', photo: ''});
+
+    function onChangeHandler(e) {
+        setImputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+    const isEmptyFormData = 
+        imputs.author.trim().length === 0 ||  imputs.title.trim().length === 0;
+        console.log(isEmptyFormData);
+
+        async function createBookHandler() {
+            if(isEmptyFormData) {
+                antMessage.error('Все поля обязательны к заполнению');
+            return;
+        }
+        setLoading(true);
+        try {
+            const { data, message, error, statusCode } = await BookApi.createBook(imputs);
+            if (error) {
+                antMessage.error(error);
+                return;
+            }
+            antMessage.success(message);
+            if (statusCode === 201) {
+                setBooks((prev) => [...prev, data]);
+                setImputs({author: '', title: '', photo: ''});
+            }
+
+        } catch (error) {
+            antMessage.error(error.message);
+            console.log(error)
+        } finally {
+            setLoading(false);
+        }
+    }
+   
+        return (
         <div>
-         <form>
-            <input placeholder='Название книги'></input>
-            <input placeholder='Автор'></input>
-            <input placeholder='Обложка книги'></input>
-            </form>   
-            нужен мультер для загрузки изображений
+            <input value={imputs.author} name='author' onChange={onChangeHandler} placeholder='Название книги'></input>
+            <input value={imputs.title} name='title' onChange={onChangeHandler} placeholder='Автор'></input>
+            <input value={imputs.photo} name='photo' onChange={onChangeHandler} placeholder='Добавьте обложку книги'></input>
+            <Button
+                text='Создать'
+                color='green'
+                onClick={createBookHandler}
+                disabled={isEmptyFormData}
+      />
         </div>
     );
 }
 
-export default BookForm;
+
 
 
 
