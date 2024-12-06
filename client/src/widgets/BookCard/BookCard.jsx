43d60028efbb9ea91f6ab26f020f88afad492579
Никита Матигулин <nikitamatigulin@MacBook-Pro-Nikita.local>
2024-12-06 20:00:50 +0300

    fixCRUD

--- client/src/widgets/BookCard/BookCard.jsx
+++ client/src/widgets/BookCard/BookCard.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
-import { Rate, Card, message as antMessage, Button } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import './BookCard.css'; // Создайте файл стилей для карточек
+import React, { useState } from "react";
+import { Rate, Card, message as antMessage, Button } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import "./BookCard.css"; // Создайте файл стилей для карточек
 
 function BookCard({ book, setBooks, user }) {
-
   // console.log(book);
-  
-  
+
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [showUpdateForm, setShowUpdateForm] = useState(false);
- console.log(book);
+  console.log(book);
   async function deleteBookHandler(title) {
     if (user.id !== book.userId) {
       antMessage.error(`No rights to delete book with id ${book.id}`);
@@ -19,7 +17,9 @@ function BookCard({ book, setBooks, user }) {
     }
     setLoading(true);
     try {
-      const { data, message, error, statusCode } = await BookApi.deleteBookById(book.id);
+      const { data, message, error, statusCode } = await BookApi.deleteBookById(
+        book.id
+      );
 
       if (error) {
         antMessage.error(error);
@@ -33,9 +33,8 @@ function BookCard({ book, setBooks, user }) {
       antMessage.error(error.message);
       console.log(error);
     } finally {
-     
-      antMessage.info('Загрузка завершена');
-      
+      antMessage.info("Загрузка завершена");
+
       setLoading(false);
     }
   }
@@ -43,50 +42,48 @@ function BookCard({ book, setBooks, user }) {
   function redirectButtonHandler() {
     navigate(`/books/${book.id}`);
   }
-  
-  return (
-    <Card 
-      className="book-card"
-      cover={<img alt={book.title} src={book.photo} />}
-      actions={[
-        <a key="more" href={`/books/${book.id}`} className="full-width-button">
-          <Button type="primary">Подробнее</Button>
-        </a>,
-        // user?.id === book.userId && (
-        //   <Button key="delete" type="danger" onClick={() => deleteBookHandler(book.title)}>
-        //     Удалить
-        //   </Button>
-        // ),
-        // user?.id === book.userId && (
-        //   <Button
-        //     key="update"
-        //     type="warning"
-        //     onClick={() => setShowUpdateForm((prev) => !prev)}
-        //   >
-        //     {showUpdateForm ? 'Скрыть' : 'Изменить'}
-        //   </Button>
-        // ),
-      ]}
-    >
-      <Rate disabled defaultValue={Math.floor(book.Ratings.reduce((sum, e) => sum + (e.rating || 0), 0) / book.Ratings.length) || 0} count={10} onChange={(star) => console.log(star)}/>
-      <p></p>
-      <Card.Meta
-        title={book.title}
-        description={book.author}
-      />
 
-      {showUpdateForm && user.id === book.userId && (
-        <BookUpdateForm
-          user={user}
-          book={book}
-          setBooks={setBooks}
-          setLoading={setLoading}
-          setShowUpdateForm={setShowUpdateForm}
+  return (
+    <Link to={`/books/${book.id}`}>
+      <Card
+        className="book-card"
+        cover={<img alt={book.title} src={book.photo} />}
+        actions={[
+          <Button onClick={redirectButtonHandler} type="primary">
+            Подробнее
+          </Button>,
+        ]}
+      >
+        <Rate
+          disabled
+          defaultValue={
+            Math.floor(
+              book.Ratings.reduce((sum, e) => sum + (e.rating || 0), 0) /
+                book.Ratings.length
+            ) || 0
+          }
+          count={10}
+          onChange={(star) => console.log(star)}
         />
-      )}
-      <p>Средний рейтинг: {(book.Ratings.reduce((sum, e) => sum + (e.rating || 0), 0) / book.Ratings.length) || 'Нет оценок'}</p>
-    </Card>
+        <Card.Meta title={book.title} description={book.author} />
+        {showUpdateForm && user.id === book.userId && (
+          <BookUpdateForm
+            user={user}
+            book={book}
+            setBooks={setBooks}
+            setLoading={setLoading}
+            setShowUpdateForm={setShowUpdateForm}
+          />
+        )}
+        <p>
+          Средний рейтинг:{" "}
+          {book.Ratings.reduce((sum, e) => sum + (e.rating || 0), 0) /
+            book.Ratings.length || "Нет оценок"}
+        </p>
+      </Card>
+    </Link>
   );
 }
+
 
 export default BookCard;
